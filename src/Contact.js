d30099e204826eee0a14cacc1eb04cb3073dfa72
Paul Tsnobiladzé <paul.tsnobiladze@gmail.com>
2020-10-19 12:20:27 +0200

    add links to github and contributions

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-import { View, Text, StyleSheet, Svg, Path } from "@react-pdf/renderer"
-import { innerMargin, Separator, defaultFontSize } from "./Styles"
+import { View, Text, Link, StyleSheet, Svg, Path } from "@react-pdf/renderer"
+import { defaultTextColor, innerMargin, Separator, defaultFontSize } from "./Styles"
 import CategoryTitle from "./CategoryTitle"
 
 const styles = StyleSheet.create({
@@ -22,7 +22,9 @@ const styles = StyleSheet.create({
         marginLeft: "6mm",
         fontSize: defaultFontSize,
         fontFamily: "Roboto",
-        fontWeight: "normal"
+        fontWeight: "normal",
+        color: defaultTextColor,
+        textDecoration: "unset"
     }
 })
 
@@ -41,20 +43,27 @@ const githubLogo = <Svg width={logoSize} height={logoSize} viewBox="0 0 16 16" x
     <Path fill-rule="evenodd" clip-rule="evenodd" d="M7.976 0A7.977 7.977 0 0 0 0 7.976c0 3.522 2.3 6.507 5.431 7.584.392.049.538-.196.538-.392v-1.37c-2.201.49-2.69-1.076-2.69-1.076-.343-.93-.881-1.175-.881-1.175-.734-.489.048-.489.048-.489.783.049 1.224.832 1.224.832.734 1.223 1.859.88 2.3.685.048-.538.293-.88.489-1.076-1.762-.196-3.621-.881-3.621-3.964 0-.88.293-1.566.832-2.153-.05-.147-.343-.978.098-2.055 0 0 .685-.196 2.201.832.636-.196 1.322-.245 2.007-.245s1.37.098 2.006.245c1.517-1.027 2.202-.832 2.202-.832.44 1.077.146 1.908.097 2.104a3.16 3.16 0 0 1 .832 2.153c0 3.083-1.86 3.719-3.62 3.915.293.244.538.733.538 1.467v2.202c0 .196.146.44.538.392A7.984 7.984 0 0 0 16 7.976C15.951 3.572 12.38 0 7.976 0z" />
 </Svg>
 
-const ContactLine = ({ logo, children }) =>
-    <View style={styles.contactLine}>
-        {logo}
-        <Text style={styles.contact}>{children}</Text>
-    </View>
+
+// todo remove the added character (!) in src when bug is fixed in @react-pdf/renderer
+const ContactLine = ({ url, logo, children }) =>
+    url ?
+        <Link src={"!" + url} style={styles.contactLine}>
+            {logo}
+            <Text style={styles.contact}>{children}</Text>
+        </Link> :
+        <View style={styles.contactLine} >
+            {logo}
+            <Text style={styles.contact}>{children}</Text>
+        </View>
 
 const Contact = () => <View style={styles.card}>
     <CategoryTitle>Contact</CategoryTitle>
     <View style={styles.inner}>
-        <ContactLine logo={githubLogo}>tsnobip</ContactLine>
+        <ContactLine url="https://github.com/tsnobip" logo={githubLogo}>tsnobip</ContactLine>
         <Separator verticalMargin={8} />
-        <ContactLine logo={phoneLogo}>+33 6 75 05 61 61</ContactLine>
+        <ContactLine url="tel:+33675056161" logo={phoneLogo}>+33 6 75 05 61 61</ContactLine>
         <Separator verticalMargin={8} />
-        <ContactLine logo={emailLogo}>paul.tsnobiladze@gmail.com</ContactLine>
+        <ContactLine url="mailto:paul.tsnobiladze@gmail.com" logo={emailLogo}>paul.tsnobiladze@gmail.com</ContactLine>
     </View>
 </View>
 
--- src/Contributions.js
+++ src/Contributions.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 
-import { View, Text, StyleSheet } from "@react-pdf/renderer"
+import { View, Text, Link, StyleSheet } from "@react-pdf/renderer"
 import { innerMargin } from "./Styles"
 import CategoryTitle from "./CategoryTitle"
 import { defaultFontSize, defaultLineHeight } from "./Styles"
@@ -21,17 +21,17 @@ const styles = StyleSheet.create({
     }
 })
 
-const Contribution = ({ children }) => <Text style={styles.skill}>{children}</Text>
+// todo remove the added character (!) in src when bug is fixed in @react-pdf/renderer
+const Contribution = ({ url, children }) => <Link style={styles.skill} src={"!" + url} >{children}</Link>
 
 const DevSkills = () => <View style={styles.card}>
     <CategoryTitle>Main public contributions</CategoryTitle>
     <View style={styles.inner}>
-        <Contribution>zth/reason-relay</Contribution>
-        <Contribution>rescript/rescript-compiler - Belt</Contribution>
-        <Contribution>wearegoat/bs-prosemirror</Contribution>
-        <Contribution></Contribution>
-        <Contribution>ocaml/ocaml-lsp</Contribution>
-        <Contribution>bs-webapi-incubator</Contribution>
+        <Contribution url="https://github.com/zth/reason-relay/pulls?q=author%3Atsnobip+is%3Apr">zth/reason-relay</Contribution>
+        <Contribution url="https://github.com/rescript-lang/rescript-compiler/pulls?q=author%3Atsnobip+is%3Apr">rescript-lang/rescript-compiler - Belt</Contribution>
+        <Contribution url="https://github.com/wearegoat/bs-prosemirror/pulls?q=author%3Atsnobip+is%3Apr">wearegoat/bs-prosemirror</Contribution>
+        <Contribution url="https://github.com/ocaml/ocaml-lsp/pull/112">ocaml/ocaml-lsp</Contribution>
+        <Contribution url="https://github.com/reasonml-community/bs-webapi-incubator/pull/165">bs-webapi-incubator</Contribution>
     </View>
 </View>
 
